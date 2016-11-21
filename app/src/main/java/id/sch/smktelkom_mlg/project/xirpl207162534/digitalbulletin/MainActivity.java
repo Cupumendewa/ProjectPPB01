ecfed32d49beec7fa8cd2fbf58273054cc54dace
mfaishalakbar <faisalwin7@gmail.com>
2016-11-21 17:22:00 +0700

    Changing Packages. Rebuild From Zero

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl207162534/digitalbulletin/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl207162534.digitalbulletin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
