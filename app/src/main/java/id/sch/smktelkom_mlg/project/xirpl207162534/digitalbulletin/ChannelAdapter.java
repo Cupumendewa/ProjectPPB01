54558585940c0ed70d5921b789ced22a77fc0a23
mfaishalakbar <faisalwin7@gmail.com>
2016-11-22 11:21:53 +0700

    Implements Hold Subscribed Channel to Delete.

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl207162534/digitalbulletin/ChannelAdapter.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl207162534/digitalbulletin/ChannelAdapter.java
@@ -1,6 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl207162534.digitalbulletin;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,7 +25,7 @@ public class ChannelAdapter extends ArrayAdapter<Channel> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
-        Channel channel = getItem(position);
+        final Channel channel = getItem(position);
         // Check if an existing view is being reused, otherwise inflate the view
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_channel, parent, false);
@@ -31,6 +34,40 @@ public class ChannelAdapter extends ArrayAdapter<Channel> {
         TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
         // Populate the data into the template view using the data object
         tvName.setText(channel.Name);
+        LinearLayout ll = (LinearLayout) convertView.findViewById(R.id.layoutN);
+        ll.setTag(position);
+        ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = (Integer) v.getTag();
+                Channel ch = getItem(position);
+                Log.d("Press",ch.id.toString());
+            }
+        });
+        ll.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int position = (Integer) v.getTag();
+                Channel ch = getItem(position);
+
+                DialogInterface.OnClickListener dialogClick = new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which){
+                            case DialogInterface.BUTTON_POSITIVE:
+                                Log.d("APP","Removed");
+                                break;
+
+                            case DialogInterface.BUTTON_NEGATIVE:
+                                break;
+                        }
+                    }
+                };
+                AlertDialog.Builder prompt = new AlertDialog.Builder(getContext());
+                prompt.setMessage("Are you Sure want to delete " + ch.Name + " from your Subscribed List?").setPositiveButton("Yes",dialogClick).setNegativeButton("No",dialogClick).show();
+                return true;
+            }
+        });
         // Return the completed view to render on screen
         return convertView;
     }
