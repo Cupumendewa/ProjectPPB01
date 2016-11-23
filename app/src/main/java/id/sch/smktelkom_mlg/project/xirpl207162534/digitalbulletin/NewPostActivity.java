31d634ab9b42432704c1e23ec21f83bc92640561
YusrilRamadhan <yusril_ramadhan_24rpl@student.smktelkom-mlg.sch.id>
2016-11-23 12:26:35 +0700

    Firebase New Post

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl207162534/digitalbulletin/NewPostActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl207162534/digitalbulletin/NewPostActivity.java
@@ -3,12 +3,17 @@ package id.sch.smktelkom_mlg.project.xirpl207162534.digitalbulletin;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class NewPostActivity extends AppCompatActivity {
     EditText title, content;
@@ -50,7 +55,18 @@ public class NewPostActivity extends AppCompatActivity {
         if(valid()){
             int postcount;
             FirebaseDatabase db = FirebaseDatabase.getInstance();
-            DatabaseReference dbref = db.getReference("channels").child("id").child("posts");
+            DatabaseReference dbref = db.getReference("channels").child(id).child("posts");
+            dbref.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    Log.d("APP",String.valueOf(dataSnapshot.getChildrenCount()));
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
     }
 
