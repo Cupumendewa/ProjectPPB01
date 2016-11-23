e062acdba6b834a053355a0cdb2e41ffad5a3861
YusrilRamadhan <yusril_ramadhan_24rpl@student.smktelkom-mlg.sch.id>
2016-11-23 12:23:03 +0700

    Firebase New Post

--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl207162534/digitalbulletin/NewPostActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl207162534/digitalbulletin/NewPostActivity.java
@@ -1,14 +1,19 @@
 package id.sch.smktelkom_mlg.project.xirpl207162534.digitalbulletin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class NewPostActivity extends AppCompatActivity {
     EditText title, content;
     Button submit;
+    String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,8 @@ public class NewPostActivity extends AppCompatActivity {
         title = (EditText) findViewById(R.id.newpostTitle);
         content = (EditText) findViewById(R.id.newpostContent);
         submit = (Button) findViewById(R.id.submitNewPost);
-
+        Intent intent = getIntent();
+        id = intent.getStringExtra("id");
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,7 +48,9 @@ public class NewPostActivity extends AppCompatActivity {
 
     private void post(){
         if(valid()){
-
+            int postcount;
+            FirebaseDatabase db = FirebaseDatabase.getInstance();
+            DatabaseReference dbref = db.getReference("channels").child("id").child("posts");
         }
     }
 
